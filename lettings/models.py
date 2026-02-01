89e2706558e40ba06ef3ac8e8af668081d332a8e
Jeremy Muller <jeremymuller92@gmail.com>
2026-02-01 11:35:01 +0100

    Worked out the migration of the tables by pointing to the old ones via Meta db_tables = "".

--- lettings/models.py
+++ lettings/models.py
@@ -9,14 +9,22 @@ class Address(models.Model):
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+    related_name = "new_address"
 
     def __str__(self):
         return f"{self.number} {self.street}"
+
+    class Meta:
+        db_table = "oc_lettings_site_address"
 
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    related_name = "new_letting"
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = "oc_lettings_site_letting"
--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="new_user")
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
         return self.user.username
+
+    class Meta:
+        db_table = "oc_lettings_site_profile"
