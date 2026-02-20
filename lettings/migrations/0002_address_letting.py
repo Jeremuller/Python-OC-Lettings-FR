67117421f2e09f64736950cb08be8df9a980673e
Jeremy Muller <jeremymuller92@gmail.com>
2026-02-20 12:24:38 +0100

    last migration and update for architecture refactoring

--- lettings/migrations/0002_address_letting.py
+++ lettings/migrations/0002_address_letting.py
@@ -10,34 +10,69 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('lettings', '0001_initial'),
+        ("lettings", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Address',
+            name="Address",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('number', models.PositiveIntegerField(validators=[django.core.validators.MaxValueValidator(9999)])),
-                ('street', models.CharField(max_length=64)),
-                ('city', models.CharField(max_length=64)),
-                ('state', models.CharField(max_length=2, validators=[django.core.validators.MinLengthValidator(2)])),
-                ('zip_code', models.PositiveIntegerField(validators=[django.core.validators.MaxValueValidator(99999)])),
-                ('country_iso_code', models.CharField(max_length=3, validators=[django.core.validators.MinLengthValidator(3)])),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                (
+                    "number",
+                    models.PositiveIntegerField(
+                        validators=[django.core.validators.MaxValueValidator(9999)]
+                    ),
+                ),
+                ("street", models.CharField(max_length=64)),
+                ("city", models.CharField(max_length=64)),
+                (
+                    "state",
+                    models.CharField(
+                        max_length=2, validators=[django.core.validators.MinLengthValidator(2)]
+                    ),
+                ),
+                (
+                    "zip_code",
+                    models.PositiveIntegerField(
+                        validators=[django.core.validators.MaxValueValidator(99999)]
+                    ),
+                ),
+                (
+                    "country_iso_code",
+                    models.CharField(
+                        max_length=3, validators=[django.core.validators.MinLengthValidator(3)]
+                    ),
+                ),
             ],
             options={
-                'db_table': 'oc_lettings_site_address',
+                "db_table": "oc_lettings_site_address",
             },
         ),
         migrations.CreateModel(
-            name='Letting',
+            name="Letting",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=256)),
-                ('address', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='lettings.Address')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("title", models.CharField(max_length=256)),
+                (
+                    "address",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE, to="lettings.Address"
+                    ),
+                ),
             ],
             options={
-                'db_table': 'oc_lettings_site_letting',
+                "db_table": "oc_lettings_site_letting",
             },
         ),
     ]
