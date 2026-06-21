38b1c9eb465e1c627aa12736958f506c503ccede
Jeremy Muller <jeremymuller92@gmail.com>
2026-06-21 13:08:11 +0200

    Created a datajson to initialize datas in render postgre service, and a command in starting script to load it

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
+echo "Loading initial data..."
+python manage.py loaddata fixtures/initial_data.json
+
 echo "Starting Gunicorn..."
 exec gunicorn oc_lettings_site.wsgi:application \
     --bind 0.0.0.0:${PORT:-8000}
